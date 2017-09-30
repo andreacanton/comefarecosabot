951bbcfc1453749fb7f48f78cf0d2aee772f2e43
Andrea Canton <andrea.canton@triboo.it>
2017-09-30 03:19:24 +0200

    Add notes for future implementations

--- bot.js
+++ bot.js
@@ -13,18 +13,21 @@ else {
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
-bot.onText(/Ciao/, function (msg) {
+bot.onText(/Ciao/i, function (msg) {
   var name = msg.from.first_name;
   bot.sendMessage(msg.chat.id, 'Ciao, ' + name + '!').then(function () {
     // reply sent!
   });
 });
 
-bot.onText(/Come stai\?/, function (msg) {
+bot.onText(/Come stai\?/i, function (msg) {
   var name = msg.from.first_name;
   bot.sendMessage(msg.chat.id, 'Benino, ' + name + ' e tu?').then(function () {
     // reply sent!
   });
 });
+
+// telegramBot.sendVideoNote da implementare
+// telegramBot.sendVideo per video più lunghi di 1 minuto
 
 module.exports = bot;
